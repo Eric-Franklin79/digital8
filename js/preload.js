93f29e05b1b66617155b909e1f183e2a06180d9c
Eric-Franklin79 <pickle797900@verizon.net>
2015-04-06 21:18:28 -0400

    done

--- js/preload.js
+++ js/preload.js
@@ -9,10 +9,20 @@ PreloadState.prototype = {
 		loading.animations.play('move');
 		
 		//load everything else
-		this.game.laod.image('canvasBackground', ' assets/canvas.png');
+		this.game.load.image('canvasBackground', 'assets/canvas.png');
 		this.game.load.image('car', 'assets/car.png');
+		this.game.load.image('line', 'assets/lineButton.png');
+		this.game.load.image('right', 'assets/right.png');
+		this.game.load.image('left', 'assets/left.png');
+		this.game.load.image('redB', 'assets/red.png');
+		this.game.load.image('greenB', 'assets/green.png');
+		this.game.load.image('blueB', 'assets/blue.png');
+		this.game.load.image('yellowB', 'assets/yellow.png');
+		this.game.load.image('blackB', 'assets/black.png');
+		this.game.load.image('whiteB', 'assets/white.png');
+		this.game.load.image('doneB', 'assets/doneButton.png');
 	},
 	create: function(){
-		this.game.state.start("menu");
+		this.game.state.start("canvas");
 	}
 }
